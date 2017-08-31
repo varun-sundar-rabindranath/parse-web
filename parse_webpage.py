0c308676cbed0d35c4e20769421dc49f14a68662
Varun Sundar Rabindranath <varunsundar08@gmail.com>
2017-08-31 16:26:50 -0400

    [dev] Add help

--- parse_webpage.py
+++ parse_webpage.py
@@ -3,7 +3,26 @@
 # webpage. We will split the file into packages later, as each module grows
 
 import urllib.request # Get raw data from a webpage
+import sys            # Parse input arguments
 
-html = urllib.request.urlopen("https://www.yelp.com/careers/job-openings/0aa8d9cb-776a-44d4-9fba-f9ed632e6ead?description=Software-Engineer-Data-Mining-NLP_Engineering_San-Francisco-CA").read()
+# 'help' prints information on how to use the program
+def help():
+    help_str = '''
+    The program expects 1 argument that is the URL to parse.
+
+    Example:
+        python3 parse_webpage.py "https://pythonspot.com/en/urllib-tutorial-python-3/"
+        '''
+    print(help_str)
+
+# Get input arguments that is a webpage
+if (len(sys.argv) != 2):
+    print("Insufficient Arguments")
+    help()
+    sys.exit(-1)
+
+url = sys.argv[1]
+
+html = urllib.request.urlopen(url).read()
 
 print(html)
